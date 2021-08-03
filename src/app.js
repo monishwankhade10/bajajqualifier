e149458d63e0bb8051d3c28251d2ed4c4a6352a4
monishwankhade10 <monwan786@gmail.com>
2021-08-03 10:11:33 +0530

    starter

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const app=express();
+app.use(express.json());
+let port=process.env.PORT;
+// let PORT=8080;
+app.get("/bfhl",(req,res)=>{
+    res.status(200).json({"monish":"monish"});
+})
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+})
